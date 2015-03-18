c771fba696d32a6e90c8959d363fe1e9960501cf
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-03-18 09:24:17 +0200

    Task, Task<T>, async, await

---
+++ TalentManager/TalentManager/Handlers/MyHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TalentManager.Handlers
+{
+    public class MyHandler : DelegatingHandler
+    {
+        // http://www.dotnetperls.com/async
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            // Inspect and do your stuff with request here
+
+            // If you are not happy for any reason,
+            // you can reject the request right here like this
+
+            bool isBadRequest = false;
+            if (isBadRequest)
+                return request.CreateResponse(HttpStatusCode.BadRequest);
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            // Inspect and do your stuff with response here
+
+            return response;
+        }
+    }
+}
--- TalentManager/TalentManager/Handlers/MyHandler40.cs
+++ TalentManager/TalentManager/Handlers/MyHandler40.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -10,7 +11,9 @@ namespace TalentManager.Handlers
 {
     public class MyHandler40 : DelegatingHandler // Handler for .NET 4.0
     {
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        // SendAync returns HttpResponseMessage Delegate
+        // http://www.codeproject.com/Articles/189374/The-Basics-of-Task-Parallelism-via-C
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             // Inspect and do your stuff with request here
 
