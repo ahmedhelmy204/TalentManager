7612e0265aab099415843677b15784dd6aa6f178
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-03-18 08:51:41 +0200

    Page 35 - Listing 3-4

---
+++ TalentManager/TalentManager/Handlers/MyHandler40.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TalentManager.Handlers
+{
+    public class MyHandler40 : DelegatingHandler // Handler for .NET 4.0
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            // Inspect and do your stuff with request here
+
+            // If you are not happy for any reason,
+            // you can reject the request right here like this
+
+            bool isBadRequest = false;
+            if (isBadRequest)
+            {
+                return Task<HttpResponseMessage>.Factory.StartNew(() =>
+                    {
+                        return request.CreateResponse(HttpStatusCode.BadRequest);
+                    });
+            }
+
+            return base.SendAsync(request, cancellationToken)
+                .ContinueWith((task) =>
+                {
+                    var response = task.Result;
+
+                    // Inspect and do your stuff with response here
+                    return response;
+                });
+        }
+    }
+}
