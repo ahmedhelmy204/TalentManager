e325f94b426fcf927d9c44225bed057386732101
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-04-21 11:10:12 +0200

    Listing 4-6

--- TalentManager/TalentManager/Controllers/EmployeesController.cs
+++ TalentManager/TalentManager/Controllers/EmployeesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using TalentManager.Models;
 
@@ -49,6 +50,35 @@ namespace TalentManager.Controllers
             }
 
             return new Employee() { Id = id, Name = "John Q Law", Department = "Enforcement" };
+        }
+
+        public HttpResponseMessage GetAllEmployees()
+        {
+            var employees = new Employee[]{
+                new Employee()
+                {
+                    Id=12345,
+                    Name="John Q Law",
+                    Department="Enforcement"
+                },
+                new Employee()
+                {
+                        Id=45678,
+                        Name="Jane Q Taxpayer",
+                        Department="Revenue"
+                }
+            };
+
+            var response = Request.CreateResponse<IEnumerable<Employee>>(HttpStatusCode.OK, employees);
+
+            response.Headers.CacheControl = new CacheControlHeaderValue()
+            {
+                MaxAge = TimeSpan.FromSeconds(6),
+                MustRevalidate = true,
+                Private = true
+            };
+
+            return response;
         }
 
         public Employee Post(Employee human)
